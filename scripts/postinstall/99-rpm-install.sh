9b9ae97e6e27ef0c6eee52482ba6f0e28760a06e
Valentin Geffroy <valentin.geffroy@iot.bzh>
2025-06-17 09:19:18 +0200

    pkgs: Add script for RPM installation
    Which are not included in redpesk batz (cent OS 9)
    
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

---
+++ scripts/postinstall/99-rpm-install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# systemd-networkd not present in redpesk so manually install it
+dnf --installroot="$BUILDROOT" install -y https://dl.fedoraproject.org/pub/epel/9/Everything/aarch64/Packages/s/systemd-networkd-253.4-1.el9.aarch64.rpm
