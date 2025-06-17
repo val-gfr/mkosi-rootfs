397197b56788c9123058eceb71e1d0537d042fbf
Valentin Geffroy <valentin.geffroy@iot.bzh>
2025-06-17 10:55:30 +0200

    pkgs: systemd-networkd: Add basic dhcp config
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

--- scripts/postinstall/99-rpm-install.sh
+++ scripts/postinstall/99-rpm-install.sh
@@ -3,5 +3,14 @@
 # systemd-networkd not present in redpesk so manually install it
 dnf --installroot="$BUILDROOT" install -y https://dl.fedoraproject.org/pub/epel/9/Everything/aarch64/Packages/s/systemd-networkd-253.4-1.el9.aarch64.rpm
 
+# systemd-networkd configuration (DHCP eth0)
+cat << EOF > $BUILDROOT/etc/systemd/network/01-eth0.network
+[Match]
+Name=eth0
+
+[Network]
+DHCP=ipv4
+EOF
+
 # ncdu not present in redpesk so manually install it
 dnf --installroot="$BUILDROOT" install -y https://dl.fedoraproject.org/pub/epel/9/Everything/aarch64/Packages/n/ncdu-1.22-1.el9.aarch64.rpm
