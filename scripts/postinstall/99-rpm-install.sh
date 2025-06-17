029578ca97b5bb562677d66b754c3a30a1340434
Valentin Geffroy <valentin.geffroy@iot.bzh>
2025-06-17 10:05:46 +0200

    pkgs: Add ncdu for size measures on rootfs
    Will be removed later, here for development purposes.
    
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

--- scripts/postinstall/99-rpm-install.sh
+++ scripts/postinstall/99-rpm-install.sh
@@ -2,3 +2,6 @@
 
 # systemd-networkd not present in redpesk so manually install it
 dnf --installroot="$BUILDROOT" install -y https://dl.fedoraproject.org/pub/epel/9/Everything/aarch64/Packages/s/systemd-networkd-253.4-1.el9.aarch64.rpm
+
+# ncdu not present in redpesk so manually install it
+dnf --installroot="$BUILDROOT" install -y https://dl.fedoraproject.org/pub/epel/9/Everything/aarch64/Packages/n/ncdu-1.22-1.el9.aarch64.rpm
