3ec7a3b7aec304823401e9d4a06381e9925ff6d6
Valentin Geffroy <valentin.geffroy@iot.bzh>
2025-07-09 16:19:36 +0200

    scripts: Add remove package after image build
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

---
+++ scripts/postinstall/80-remove-pkgs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+## remove redpesk packages which are not needed
+# dnf --installroot="$BUILDROOT" remove -y \
+#     redpesk-repos-config \
+#     redpesk-repos \
+#     redpesk-config \
+#     redpesk-gpg-keys \
+#     redpesk-release
+
+## remove others packages
+
+exit 0
