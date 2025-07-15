80a183445794ec0aa5431dd10f12b09565023444
Valentin Geffroy <valentin.geffroy@iot.bzh>
2025-07-15 15:27:09 +0200

    pkgs: Add systemd-networkd again
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

--- scripts/postinstall/90-rpm-install.sh
+++ scripts/postinstall/90-rpm-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # systemd-networkd not present in redpesk so manually install it
-# dnf --installroot="$BUILDROOT" install -y https://dl.fedoraproject.org/pub/epel/9/Everything/aarch64/Packages/s/systemd-networkd-253.4-1.el9.aarch64.rpm
+dnf --installroot="$BUILDROOT" install -y https://dl.fedoraproject.org/pub/epel/9/Everything/aarch64/Packages/s/systemd-networkd-253.4-1.el9.aarch64.rpm
 
 ## install packages which have been previously removed
 dnf --installroot="$BUILDROOT" install -y \
