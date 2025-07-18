02dc05e39aee7125de27340e0194b59995503dfa
Valentin Geffroy <valentin.geffroy@iot.bzh>
2025-07-18 17:33:00 +0200

    network: Replace systemd-networkd by iproute
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

--- scripts/postinstall/90-rpm-install.sh
+++ scripts/postinstall/90-rpm-install.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-# systemd-networkd not present in redpesk so manually install it
-dnf --installroot="$BUILDROOT" install -y https://dl.fedoraproject.org/pub/epel/9/Everything/aarch64/Packages/s/systemd-networkd-253.4-1.el9.aarch64.rpm
+# iproute & iproute-tc instead of systemd-networkd
+dnf --installroot="$BUILDROOT" install -y \
+	iproute \
+	iproute-tc
 
 ## install packages which have been previously removed
 dnf --installroot="$BUILDROOT" install -y \
