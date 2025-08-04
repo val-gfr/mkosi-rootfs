336b66cb0ef003ef70eb5dc61c8435de61cd3879
Valentin Geffroy <valentin.geffroy@iot.bzh>
2025-08-04 17:35:47 +0200

    boot: Add custom script before systemd launch
    Keeping PID1 by using exec () instruction call
    
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

--- scripts/postinstall/92-cleanup.sh
+++ scripts/postinstall/92-cleanup.sh
@@ -11,6 +11,17 @@ rm -f $BUILDROOT/var/lib/rpm/__db*
 # Build image date in /etc/os-release
 echo "BUILD_DATE=\"$(TZ='UTC+2' date '+%Y-%m-%d %H:%M:%S')\"" >> $BUILDROOT/etc/os-release
 
+# Replace systemd init by custom one
+unlink $BUILDROOT/init
+
+cat << EOF > $BUILDROOT/init
+#!/bin/sh
+/usr/bin/echo "Custom boot test"
+exec /usr/lib/systemd/systemd
+EOF
+
+chmod +x $BUILDROOT/init
+
 # setup systemd to boot to the right runlevel
 #echo -n "Setting default runlevel to multiuser text mode"
 #rm -f $BUILDROOT/etc/systemd/system/default.target
