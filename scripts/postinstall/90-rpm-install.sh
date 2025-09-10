305cb17fd78b8295d2691d03073dfb047cf6b3a8
Valentin Geffroy <valentin.geffroy@iot.bzh>
2025-09-10 15:38:08 +0200

    pkgs: Add Network for external package (ncdu)
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

--- scripts/postinstall/90-rpm-install.sh
+++ scripts/postinstall/90-rpm-install.sh
@@ -21,3 +21,5 @@ dnf --installroot="$BUILDROOT" install -y \
 
 # ncdu not present in redpesk so manually install it
 dnf --installroot="$BUILDROOT" install -y https://dl.fedoraproject.org/pub/epel/9/Everything/aarch64/Packages/n/ncdu-1.22-1.el9.aarch64.rpm
+
+exit 0
