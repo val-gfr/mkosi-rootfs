b140ac064aafda2b525bc1b36b78d10504036939
Valentin Geffroy <valentin.geffroy@iot.bzh>
2025-09-22 17:57:50 +0200

    clean: Some rework of removed packages
    Better efficiency for cleaning packages phase.
    
    Signed-off-by: Valentin Geffroy <valentin.geffroy@iot.bzh>

--- scripts/postinstall/92-remove-pkgs.sh
+++ scripts/postinstall/92-remove-pkgs.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 ## remove redpesk packages which are not needed
-# dnf --installroot="$BUILDROOT" remove -y \
+#dnf --installroot="$BUILDROOT" remove -y \
 #     redpesk-repos-config \
 #     redpesk-repos \
 #     redpesk-config \
 #     redpesk-gpg-keys \
 #     redpesk-release
 
-## remove others packages and their dependancies
+## remove others packages
 dnf --installroot="$BUILDROOT" remove -y \
       kbd
 
@@ -19,5 +19,10 @@ dnf --installroot="$BUILDROOT" remove -y --setopt=clean_requirements_on_remove=0
 ## only remove alternatives
 dnf --installroot="$BUILDROOT" remove -y --setopt=clean_requirements_on_remove=0 \
       libselinux-utils
+
+## only remove shadow-utils
+dnf --installroot="$BUILDROOT" remove -y --setopt=clean_requirements_on_remove=0 \
+      passwd \
+      shadow-utils
 
 exit 0
